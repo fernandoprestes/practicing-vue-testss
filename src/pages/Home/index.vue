78ca14ab658e3ac4a3f1ceede214dbc0b26ce4d8
fernandoprestes <fernandoprestesc@gmail.com>
2022-05-20 21:17:45 -0300

    fix: removed files obsolete

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,83 +1,20 @@
 <template>
-  <div class="container flex h-screen flex-col items-center justify-center space-y-10">
-    <h1 class="text-4xl font-bold text-primary-300">Boilerplate Vite + Vue3</h1>
-    <div class="flex gap-4">
-      <img
-        class="w-32"
-        src="../../assets/img/vitejs.svg"
-        alt="vitejs"
-        title="vitejs"
-      />
-      <img
-        class="w-32"
-        src="../../assets/img/vue.svg"
-        alt="vue"
-        title="vue"
-      />
+  <div class="container flex h-screen items-center justify-center">
+    <div class="flex w-full flex-col justify-center md:flex-row">
+      <div class="w-full p-4 md:w-1/2">
+        <router-link
+          to="/"
+          class="font-bold text-white"
+          >Home</router-link
+        >
+        <h2 class="text-center text-xl">Lista de exercicios</h2>
+        <ul class="flex flex-col">
+          <router-link to="/price-of-land">01. O preço de um terreno</router-link>
+        </ul>
+      </div>
+      <div class="flex w-full justify-center p-4 md:w-1/2">
+        <router-view />
+      </div>
     </div>
-    <div class="flex gap-4">
-      <img
-        class="w-10"
-        src="../../assets/img/javascript.svg"
-        alt="javascript"
-        title="javascript"
-      />
-      <img
-        class="w-10"
-        src="../../assets/img/tailwindcss.svg"
-        alt="tailwindcss"
-        title="tailwindcss"
-      />
-
-      <img
-        class="w-10"
-        src="../../assets/img/eslint.svg"
-        alt="eslint"
-        title="eslint"
-      />
-      <img
-        class="w-10"
-        src="../../assets/img/prettier.svg"
-        alt="prettier"
-        title="prettier"
-      />
-    </div>
-    <div class="diver__background h-1 w-1/2"></div>
-    <div>
-      <router-link
-        class="rounded bg-primary-600 px-6 py-2 text-white transition ease-in hover:bg-primary-500"
-        :to="{ name: 'About' }"
-        >Sobre</router-link
-      >
-    </div>
-    <a
-      href="https://github.com/fernandoprestes/boilerplate-vue3-js"
-      target="_blank"
-      class="flex items-center justify-center gap-2 rounded border border-primary-600 px-2 text-white hover:bg-primary-600"
-    >
-      Github
-      <Icon name="Github" />
-    </a>
   </div>
 </template>
-<script>
-  import Icon from '../../components/Icon/Index.vue';
-  export default {
-    name: 'App',
-    components: { Icon },
-  };
-</script>
-<style
-  lang="css"
-  scoped
->
-  .diver__background {
-    background: linear-gradient(
-      100deg,
-      rgba(44, 11, 97, 0.3) 0%,
-      rgba(44, 11, 97, 0.7) 20%,
-      rgba(208, 69, 121, 0.7) 80%,
-      rgba(208, 69, 121, 0.3) 100%
-    );
-  }
-</style>
